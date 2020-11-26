6ee596cedf8e701236547f63d3ca784a3f535891
Administrator <zshanji0125@163.com>
2020-11-26 22:39:41 +0800

    connect

--- text/read/model_train.py
+++ text/read/model_train.py
@@ -2,11 +2,18 @@ import pandas as pd
 import numpy as np
 import utils.data_utils as du
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, Dense, LSTM
+
 
 #열 길이 설정
 pd.set_option("display.max_colwidth",200)
 # 파일 경로
-path = 'C:\\Users\\user\\Desktop\\edu\\reviews_.json'
+#path = 'C:\\Users\\user\\Desktop\\edu\\reviews_.json'
+path = 'D:\\reviews_.json'
 
 """
 파일 읽어오기
@@ -43,25 +50,64 @@ def change_review_data():
     return text
 
 
-def get_training_data():
-    review_text_data = change_review_data()
-    # vacabulary 크기 확인
-    t = Tokenizer()
-    t.fit_on_texts(review_text_data)
-    vocab_size = len(t.word_index) + 1
-    print(vocab_size)
-    sequences = list()
-    for line in review_text_data:
-        encoded = t.texts_to_sequences([line])[0]
-        for i in range(1,len(encoded)):
-            sequence = encoded[:i+1]
-            sequences.append(sequence)
-    print(sequences[:11])
+
+review_text_data = change_review_data()
+# vacabulary 크기 확인
+t = Tokenizer()
+t.fit_on_texts(review_text_data)
+vocab_size = len(t.word_index) + 1
+print(vocab_size)
+sequences = list()
+for line in review_text_data:
+    encoded = t.texts_to_sequences([line])[0]
+    for i in range(1,len(encoded)):
+        #sequence = encoded[:i+1]
+        sequences.append(encoded[:i+1])
+#print(sequences[:11])
+index_to_word=[]
+for key,value in t.word_index.items():
+    index_to_word[value] = key
+print('빈도수 상위 582번 단어 : {}'.format(index_to_word[582]))
+max_len = max(len(l) for l in sequences)
+print('샘플의 최대 길이 : {}'.format(max_len))
+sequences = pad_sequences(sequences, maxlen=max_len, padding='pre')
+print(sequences[:3])
+sequences = np.array(sequences)
+X = sequences[:, :-1]
+y = sequences[:, -1]
+print(X[:3])
+print(y[:3])
+y = to_categorical(y, num_classes=vocab_size)
+
+model = Sequential()
+model.add(Embedding(vocab_size, 10, input_length=max_len-1))
+# y데이터를 분리하였으므로 이제 X데이터의 길이는 기존 데이터의 길이 - 1
+model.add(LSTM(128))
+model.add(Dense(vocab_size, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, y, epochs=200, verbose=2)
 
 
+def sentence_generation(model, t, current_word, n): # 모델, 토크나이저, 현재 단어, 반복할 횟수
+    init_word = current_word # 처음 들어온 단어도 마지막에 같이 출력하기위해 저장
+    sentence = ''
+    for _ in range(n): # n번 반복
+        encoded = t.texts_to_sequences([current_word])[0] # 현재 단어에 대한 정수 인코딩
+        encoded = pad_sequences([encoded], maxlen=23, padding='pre') # 데이터에 대한 패딩
+        result = model.predict_classes(encoded, verbose=0)
+    # 입력한 X(현재 단어)에 대해서 y를 예측하고 y(예측한 단어)를 result에 저장.
+        for word, index in t.word_index.items():
+            if index == result: # 만약 예측한 단어와 인덱스와 동일한 단어가 있다면
+                break # 해당 단어가 예측 단어이므로 break
+        current_word = current_word + ' '  + word # 현재 단어 + ' ' + 예측 단어를 현재 단어로 변경
+        sentence = sentence + ' ' + word # 예측 단어를 문장에 저장
+    # for문이므로 이 행동을 다시 반복
+    sentence = init_word + sentence
+    return sentence
 
-if __name__ == '__main__':
-    get_training_data()
+print(sentence_generation(model, t, 'how', 10))
+
+
 
 
 
---
+++ utils/text.py
@@ -0,0 +1,43 @@
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('punkt')
+
+#print(word_tokenize("Don‘t be afraid.Jone’s mother is coming!"))
+
+from nltk.tokenize import WordPunctTokenizer
+#print(WordPunctTokenizer().tokenize("Don‘t be afraid. Jone’s mother is coming!"))
+
+import re
+word = re.compile(r'\W*\b\w{1,2}\b')
+#print(word.sub('','I am a student.I live Korea'))
+
+from tensorflow.keras.preprocessing.text import text_to_word_sequence
+print(text_to_word_sequence("Don‘t be afraid. Jone’s mother is coming!"))
+
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+sentences = [['barber', 'person'], ['barber', 'good', 'person'],
+             ['barber', 'huge', 'person'], ['knew', 'secret'],
+             ['secret', 'kept', 'huge', 'secret'], ['huge', 'secret'],
+             ['barber', 'kept', 'word'], ['barber', 'kept', 'word'],
+             ['barber', 'kept', 'secret'], ['keeping', 'keeping', 'huge', 'secret', 'driving', 'barber', 'crazy'],
+             ['barber', 'went', 'huge', 'mountain']]
+
+tokenizer = Tokenizer()
+# fit_on_texts()안에 코퍼스를 입력으로 하면 빈도수를 기준으로 단어 집합을 생성한다.
+tokenizer.fit_on_texts(sentences)
+#텍스트 시퀀스의 모든 단어들을 각 정수로 맵핑
+encoded = tokenizer.texts_to_sequences(sentences)
+#print(encoded)
+
+#패딩 하기 위하여 길이가 제일 긴 문장의 길이를 계산
+max_len = max(len(item) for item in encoded)
+print(max_len)
+
+for item in encoded: # 각 문장에 대해서
+    while len(item) < max_len:   # max_len보다 작으면
+        item.append(0)
+
+padded_np = np.array(encoded)
+print(padded_np)
